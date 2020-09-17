795ba23dd67a8d65c6d9be2582746fe98f5ad001
Ananth Amar <ananth.amar@sleekr.co>
2020-09-17 23:39:23 +0700

    Create Short URL API

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -7,9 +7,21 @@ class ShortUrlsController < ApplicationController
   end
 
   def create
+    short_url = ShortUrl.new(short_urls_create_params)
+    if short_url.save
+      render json: short_url.object, status: :ok
+    else
+      render json: {errors: short_url.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+
+  def short_urls_create_params
+    params.permit(:full_url)
   end
 
 end
